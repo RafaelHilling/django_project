9ca6b59874eb95a158a3ec9be94fb8d0063970ac
unknown <rafaelhilling@gmail.com>
2022-04-26 21:13:12 -0300

    user

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import post
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'status', 'created_on')
+    list_filter = ('status',)
+    search_fields = ('title', 'content')
+    propoluated_fields={'slug':'title'}
+
+
+admin.site.register(post)
